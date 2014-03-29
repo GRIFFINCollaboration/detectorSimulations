8f4bf2f4cf46f202636f3a54389a31b6358d2bb3
r3dunlop <rdunlop@Ryans-MacBook-Pro.local>
2014-03-29 16:25:32 -0400

    Fixed Bugs in linking Griffin
    I noticed a couple of errors in CMakeLists.txt. For some reason the
    text editor I am using does not write to disk at predictable times so
    what I see is not necessarily what I am getting at that time.

--- src/RootManager.cc
+++ src/RootManager.cc
@@ -34,6 +34,8 @@ RootManager::RootManager()
 	fS3Data = new TS3Data();
 	fDetectorSpice = new DetectionSystemSpice();
 
+        //GRIFFIN Event
+        fGriffinData = new TGriffinData();
 
 	//histograms 
 	fHist = new TH1F("h","h",500,0,1800);
@@ -60,6 +62,7 @@ void RootManager::SetTree()
 {
  fOutputTree->Branch("SpiceBranch","TSpiceData",&fSpiceData);
  fOutputTree->Branch("S3Branch","TS3Data",&fS3Data);
+// fOutputTree->Branch("GriffinBranch","TGriffinData",&fGriffinData);
  
  /*
 Other detector branches goes here
@@ -74,21 +77,21 @@ void RootManager::FillHist(double temp)
 }
 
 
-void RootManager::FillG4Hit(int key,  						// integer representing the key, could be used to identify the detector
-							int pdg,			 				// integer representing the type of particle
-							double Energy, 						// depositid energy in a step
-							double Px, double Py, double Pz, 	// position vector
-							int Id, 							// original(primary) ID
-							int PrimPdg,						// primary particle definition        PDG encoding
-							double PrimEnergy,					// original(primary) energy
-							double Mx, double My, double Mz)	// primary particle momentum vector
-							{
-							/*			
-							Some other functions 
-							*/
-							//Fill the event, calculate the full deposited energy etc...
-							fGeantEvent[key].FillVectors( pdg, Energy,  Px, Py, Pz,  Id,  PrimPdg, PrimEnergy,  Mx, My, Mz);
-							}
+void RootManager::FillG4Hit(int key,  	// integer representing the key, could be used to identify the detector
+int pdg,			// integer representing the type of particle
+double Energy, 			// depositid energy in a step
+double Px, double Py, double Pz,// position vector
+int Id, 			// original(primary) ID
+int PrimPdg,			// primary particle definition        PDG encoding
+double PrimEnergy,		// original(primary) energy
+double Mx, double My, double Mz)// primary particle momentum vector
+{
+	/*			
+	Some other functions 
+	*/
+	//Fill the event, calculate the full deposited energy etc...
+	fGeantEvent[key].FillVectors( pdg, Energy,  Px, Py, Pz,  Id,  PrimPdg, PrimEnergy,  Mx, My, Mz);
+}
 
 
 
@@ -105,6 +108,9 @@ void RootManager::SortEvent(void)
 			if (key>1 && key <50) SetSpiceEvent(key);
 			if (key>50 && key <1000) SetS3Event(key);
 
+			//Griffin
+			//Need to put Griffin key here
+			
 			//other
 			//if (key>1 && key <1000) SetOtherDetectorEvent(key);
 			}
@@ -118,111 +124,120 @@ void RootManager::SortEvent(void)
 // clear the SpiceData object
 	fSpiceData->Clear();
 	fS3Data->Clear();
+	
+// clear the GriffinData object
+//	fGriffinData->Clear();
 }
 
 
 void RootManager::SetSpiceEvent(int RingSeg)
 {	
-			// treat
-			fGeantEvent.at(RingSeg).SortPrimary();			
+	// treat
+	fGeantEvent.at(RingSeg).SortPrimary();			
 
-			// get the segment and ring
-			int Seg = (RingSeg%100) ;   //?? 100 should be 12... CHECK, MHD 20Dec2013 
-			int Ring = (RingSeg-Seg)/100;
+	// get the segment and ring
+	int Seg = (RingSeg%100) ;   //?? 100 should be 12... CHECK, MHD 20Dec2013 
+	int Ring = (RingSeg-Seg)/100;
 
-			// get primary
-			// Pdg	
-			int mult = fGeantEvent.at(RingSeg).GetPrimaryPdgMult(); // inside this particular pad 
-		    for (int i = 0 ; i<mult ;  i++ )
-			fSpiceData->SetPrimaryPdg( fGeantEvent.at(RingSeg).GetPrimaryPdg(i) ) ;
+	// get primary
+	// Pdg	
+	int mult = fGeantEvent.at(RingSeg).GetPrimaryPdgMult(); // inside this particular pad 
+    for (int i = 0 ; i<mult ;  i++ )
+	fSpiceData->SetPrimaryPdg( fGeantEvent.at(RingSeg).GetPrimaryPdg(i) ) ;
 
-		  	// Energy      
-		    mult = fGeantEvent.at(RingSeg).GetPrimaryEnergyMult(); // this should be the same as above
-			for (int i = 0 ; i<mult ;  i++ )
-			{
-			fSpiceData->SetPrimaryEnergy( fGeantEvent.at(RingSeg).GetPrimaryEnergy(i) ) ;
-			}
+	// Energy      
+    mult = fGeantEvent.at(RingSeg).GetPrimaryEnergyMult(); // this should be the same as above
+	for (int i = 0 ; i<mult ;  i++ )
+	{
+	fSpiceData->SetPrimaryEnergy( fGeantEvent.at(RingSeg).GetPrimaryEnergy(i) ) ;
+	}
 
-			// Momentum
-			    mult = fGeantEvent.at(RingSeg).GetPrimaryThetaMult(); // this should be the same as above
-			for (int i = 0 ; i<mult ;  i++ )
-			{
-			fSpiceData->SetPrimaryTheta(fGeantEvent.at(RingSeg).GetPrimaryTheta(i) ) ;
-			fSpiceData->SetPrimaryPhi( fGeantEvent.at(RingSeg).GetPrimaryPhi(i) ) ;
-			}
+	// Momentum
+	    mult = fGeantEvent.at(RingSeg).GetPrimaryThetaMult(); // this should be the same as above
+	for (int i = 0 ; i<mult ;  i++ )
+	{
+	fSpiceData->SetPrimaryTheta(fGeantEvent.at(RingSeg).GetPrimaryTheta(i) ) ;
+	fSpiceData->SetPrimaryPhi( fGeantEvent.at(RingSeg).GetPrimaryPhi(i) ) ;
+	}
 
-			// get the energy 			
-			double energy = fGeantEvent.at(RingSeg).GetFullEnergy();
-			double applied_resolution = fDetectorSpice->ApplySpiceResolution(energy);
-			TVector3 pos = fGeantEvent.at(RingSeg).GetFirstHitPosition() ;
+	// get the energy 			
+	double energy = fGeantEvent.at(RingSeg).GetFullEnergy();
+	double applied_resolution = fDetectorSpice->ApplySpiceResolution(energy);
+	TVector3 pos = fGeantEvent.at(RingSeg).GetFirstHitPosition() ;
 
-			// fill the SpiceData object
-			// (Th,E)
-			fSpiceData->SetSpiceThetaEDetectorNbr(1) ; 
-			fSpiceData->SetSpiceThetaEStripNbr(Ring) ;    
-			fSpiceData->SetSpiceThetaEEnergy(energy) ;     
-			fSpiceData->SetSpiceThetaEResEnergy(applied_resolution) ;
+	// fill the SpiceData object
+	// (Th,E)
+	fSpiceData->SetSpiceThetaEDetectorNbr(1) ; 
+	fSpiceData->SetSpiceThetaEStripNbr(Ring) ;    
+	fSpiceData->SetSpiceThetaEEnergy(energy) ;     
+	fSpiceData->SetSpiceThetaEResEnergy(applied_resolution) ;
 
 
-			// (Ph,E)
-			fSpiceData->SetSpicePhiEDetectorNbr(1) ;
-			fSpiceData->SetSpicePhiEStripNbr(Seg)  ;
-			fSpiceData->SetSpicePhiEEnergy(energy) ;
-			fSpiceData->SetSpicePhiEResEnergy(applied_resolution) ;
+	// (Ph,E)
+	fSpiceData->SetSpicePhiEDetectorNbr(1) ;
+	fSpiceData->SetSpicePhiEStripNbr(Seg)  ;
+	fSpiceData->SetSpicePhiEEnergy(energy) ;
+	fSpiceData->SetSpicePhiEResEnergy(applied_resolution) ;
 
-			fSpiceData->SetPositionFirstHit(pos) ;				
+	fSpiceData->SetPositionFirstHit(pos) ;				
 }
 
 
 void RootManager::SetS3Event(int RingSeg)
 {	
-			// treat
-			fGeantEvent.at(RingSeg).SortPrimary();			
+	// treat
+	fGeantEvent.at(RingSeg).SortPrimary();			
 
-			// get the segment and ring
-			int Seg = (RingSeg%100) ;   //?? 100 should be 12... CHECK, MHD 20Dec2013 
-			int Ring = (RingSeg-Seg)/100;
+	// get the segment and ring
+	int Seg = (RingSeg%100) ;   //?? 100 should be 12... CHECK, MHD 20Dec2013 
+	int Ring = (RingSeg-Seg)/100;
 
-			// get primary
-			// Pdg	
-			int mult = fGeantEvent.at(RingSeg).GetPrimaryPdgMult(); // inside this particular pad 
-		    for (int i = 0 ; i<mult ;  i++ )
-			fS3Data->SetPrimaryPdg( fGeantEvent.at(RingSeg).GetPrimaryPdg(i) ) ;
+	// get primary
+	// Pdg	
+	int mult = fGeantEvent.at(RingSeg).GetPrimaryPdgMult(); // inside this particular pad 
+    for (int i = 0 ; i<mult ;  i++ )
+	fS3Data->SetPrimaryPdg( fGeantEvent.at(RingSeg).GetPrimaryPdg(i) ) ;
 
-		  	// Energy      
-		    mult = fGeantEvent.at(RingSeg).GetPrimaryEnergyMult(); // this should be the same as above
-			for (int i = 0 ; i<mult ;  i++ )
-			{
-			fS3Data->SetPrimaryEnergy( fGeantEvent.at(RingSeg).GetPrimaryEnergy(i) ) ;
-			}
+	// Energy      
+    mult = fGeantEvent.at(RingSeg).GetPrimaryEnergyMult(); // this should be the same as above
+	for (int i = 0 ; i<mult ;  i++ )
+	{
+	fS3Data->SetPrimaryEnergy( fGeantEvent.at(RingSeg).GetPrimaryEnergy(i) ) ;
+	}
 
-			// Momentum
-			    mult = fGeantEvent.at(RingSeg).GetPrimaryThetaMult(); // this should be the same as above
-			for (int i = 0 ; i<mult ;  i++ )
-			{
-			fS3Data->SetPrimaryTheta(fGeantEvent.at(RingSeg).GetPrimaryTheta(i) ) ;
-			fS3Data->SetPrimaryPhi( fGeantEvent.at(RingSeg).GetPrimaryPhi(i) ) ;
-			}
+	// Momentum
+	    mult = fGeantEvent.at(RingSeg).GetPrimaryThetaMult(); // this should be the same as above
+	for (int i = 0 ; i<mult ;  i++ )
+	{
+	fS3Data->SetPrimaryTheta(fGeantEvent.at(RingSeg).GetPrimaryTheta(i) ) ;
+	fS3Data->SetPrimaryPhi( fGeantEvent.at(RingSeg).GetPrimaryPhi(i) ) ;
+	}
 
-			// get the energy 			
-			double energy = fGeantEvent.at(RingSeg).GetFullEnergy();
-			TVector3 pos = fGeantEvent.at(RingSeg).GetFirstHitPosition() ;
+	// get the energy 			
+	double energy = fGeantEvent.at(RingSeg).GetFullEnergy();
+	TVector3 pos = fGeantEvent.at(RingSeg).GetFirstHitPosition() ;
 
-			// fill the S3Data object
-			// (Th,E)
-			fS3Data->SetS3ThetaEDetectorNbr(1) ; 
-			fS3Data->SetS3ThetaEStripNbr(Ring) ;    
-			fS3Data->SetS3ThetaEEnergy(energy) ;     
+	// fill the S3Data object
+	// (Th,E)
+	fS3Data->SetS3ThetaEDetectorNbr(1) ; 
+	fS3Data->SetS3ThetaEStripNbr(Ring) ;    
+	fS3Data->SetS3ThetaEEnergy(energy) ;     
 
-			// (Ph,E)
-			fS3Data->SetS3PhiEDetectorNbr(1) ;
-			fS3Data->SetS3PhiEStripNbr(Seg)  ;
-			fS3Data->SetS3PhiEEnergy(energy) ;
+	// (Ph,E)
+	fS3Data->SetS3PhiEDetectorNbr(1) ;
+	fS3Data->SetS3PhiEStripNbr(Seg)  ;
+	fS3Data->SetS3PhiEEnergy(energy) ;
 
-			fS3Data->SetPositionFirstHit(pos) ;		
+	fS3Data->SetPositionFirstHit(pos) ;		
 
 }
 
+void RootManager::SetGriffinEvent(int Crystal)
+{	
+	//Stuff goes here
+	cout << " " << endl;		
+
+}
 
 
 // set event number
@@ -230,6 +245,7 @@ void RootManager::SetEventNumber(int i )
 {
 fSpiceData->SetEventNumber(i) ;
 fS3Data->SetEventNumber(i) ;
+//fGriffinData->SetEventNumber(i);
 }		
 
 
